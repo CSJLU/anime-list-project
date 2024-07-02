b92131a045ba0b44a2933514b769751c2e83f97d
CSJLU <lucourage@gmail.com>
2024-07-02 17:36:49 -0400

    Add initial header and header title

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <header>
+            <h2 className='header-title'>Anime List</h2>
+            <h2></h2>
+        </header>
+    )
+}
+
+
+
+
+export default Header
