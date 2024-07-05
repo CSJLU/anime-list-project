2a1e3eeaa190ad5bd74d8c77e78bf2ca1773785c
CSJLU <lucourage@gmail.com>
2024-07-05 16:03:15 -0400

    Condense Header and Hero components into Home commponent

--- server/src/components/Header.jsx
+++ server/src/components/Header.jsx
@@ -8,8 +8,10 @@ const Header = () => {
             <h2 className="header-title">Anime List</h2>
             <nav>
                 <ul className="nav-links">
-                    <li>About</li>
-                    <li>Sign Up</li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/anime">Animes</a></li>
+                    <li><a href="/signup">Sign Up</a></li>
+                    <li><a href="/login">Login</a></li>
                 </ul>
             </nav>
         </header>
