d7172edf2971fea4b0e163ab6ef284b3e992bc60
CSJLU <lucourage@gmail.com>
2024-07-05 16:01:30 -0400

    Implement router and add routes

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -1,14 +1,25 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import './index.css'
+import Home from './components/Home'
+import About from './components/About'
+import AnimeList from './components/AnimeList'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 
+import './index.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+      <div className="app">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/anime" element={<AnimeList />}/>
+            <Route path="/signup" element={<SignUp />}/>
+            <Route path="/login" element={<Login />}/>
+          </Routes>
+        </Router>
+      </div>
   )
 }
 
