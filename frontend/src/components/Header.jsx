0c6600beb86be79bc35dc645ac6912c6e70857f7
CSJLU <lucourage@gmail.com>
2024-07-04 20:06:03 -0400

    Change header text

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,8 +5,13 @@ import './Header.css'
 const Header = () => {
     return (
         <header>
-            <h2 className='header-title'>Anime List</h2>
-            <h2></h2>
+            <h2 className="header-title">Anime List</h2>
+            <nav>
+                <ul className="nav-links">
+                    <li>About</li>
+                    <li>Sign Up</li>
+                </ul>
+            </nav>
         </header>
     )
 }
