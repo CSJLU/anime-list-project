eb8feb8008e1baaf54b3d506a648a85286a5aea2
CSJLU <lucourage@gmail.com>
2024-07-08 16:28:26 -0400

    Rename

--- server/src/components/AnimeList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AnimeList = () => {
-    return (
-        <h1>Test</h1>
-    )
-}
-
-export default AnimeList
--- server/src/components/SignUp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SignUp = () => {
-    return (
-        <h1>Test</h1>
-    )
-}
-
-export default SignUp
