4091aa9fdf20fc4ae2950e1048703e270c047f60
CSJLU <lucourage@gmail.com>
2024-07-08 17:16:26 -0400

    Include Flask and successful api call

---
+++ server/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/anime', methods=['GET'])
+def anime():
+    url = f'https://api.jikan.moe/v4/anime/14813'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        anime_data = response.json()
+        return jsonify(anime_data)
+    else:
+        return jsonify({'error': 'Anime not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
