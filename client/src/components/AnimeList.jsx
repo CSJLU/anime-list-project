50566b2895c6057e8770a9912e6b908b276b1025
CSJLU <lucourage@gmail.com>
2024-07-13 18:33:40 -0400

    Connect to backend to get JikanAPI data

--- client/src/components/AnimeList.jsx
+++ client/src/components/AnimeList.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useState, useEffect} from 'react' 
 
 const AnimeList = () => {
+    const [animeData, setAnimeData] = useState({})
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/anime')
+        .then(response => response.json())
+        .then(data => setAnimeData(data))
+        .catch(error => console.error(error));
+    }, []);
+    
+    console.log(animeData);
+
+    if (!animeData.data) {
+        return <div>No anime data available</div>;
+    }
+    
     return (
-        <h1>Test</h1>
+        <div>
+            <h1>Test</h1>
+            <h1> {animeData.data.title} </h1>
+            <h1> {animeData.data.synopsis} </h1>
+        </div>
     )
 }
 
