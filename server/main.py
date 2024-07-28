3866e1580b7d2e95ed4e2baf972c6509d5d937df
CSJLU <lucourage@gmail.com>
2024-07-28 16:42:58 -0400

    Not needed, will delete later

--- server/main.py
+++ server/main.py
@@ -1,21 +1,23 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-import requests
+# from flask import Flask, jsonify
+# from flask_cors import CORS
+# import requests
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
-@app.route('/api/anime', methods=['GET'])
-def anime():
-    url = f'https://api.jikan.moe/v4/anime/14813'
-    response = requests.get(url)
+# @app.route('/api/anime', methods=['GET'])
+# def anime():
+#     url = f'https://api.jikan.moe/v4/anime/14813'
+#     response = requests.get(url)
     
-    if response.status_code == 200:
-        anime_data = response.json()
-        return jsonify(anime_data)
-    else:
-        return jsonify({'error': 'Anime not found'}), 404
+#     if response.status_code == 200:
+#         anime_data = response.json()
+#         return jsonify(anime_data)
+#     else:
+#         return jsonify({'error': 'Anime not found'}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 
