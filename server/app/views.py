430c6a7127393ba505334ca5656ba622b5c71a99
CSJLU <lucourage@gmail.com>
2024-07-30 22:32:11 -0400

    Modularize code as well as accept query input to access different data endpoints

--- server/app/views.py
+++ server/app/views.py
@@ -1,14 +1,31 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 import requests
 
-@app.route('/api/anime', methods=['GET'])
-def anime():
-    url = f'https://api.jikan.moe/v4/anime/14813'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        anime_data = response.json()
-        return jsonify(anime_data)
-    else:
-        return jsonify({'error': 'Anime not found'}), 404
+JIKAN_API_BASE_URL = "https://api.jikan.moe/v4"
+
+def get_anime_from_jikan(query):
+    url = f"{JIKAN_API_BASE_URL}/anime"
+    try:
+        response = requests.get(url, params={'q': query})
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.ConnectionError as err: #for network issues
+        return jsonify({'error': 'Connection error. Please try again later.'}), 503
+    except requests.exceptions.HTTPError as err: #url, server, etc. issues (404, 500)
+        return jsonify({'error': 'An error occurred while processing the request.'}), 500
+
+
+
+@app.route('/search_anime', methods=['GET'])
+def search_anime():
+    try: 
+        query = request.args.get('q') #query from frontend is accessed through Flask request object 
+        data = get_anime_from_jikan(query)
+        return jsonify(data)
+    except requests.exceptions.ConnectionError as err:
+        return jsonify({'error': 'Connection error. Please try again later.'}), 503
+    except requests.exceptions.HTTPError as err: 
+        return jsonify({'error': 'An error occurred while processing the request.'}), 500
+    except Exception as err:
+        return jsonify(err)
