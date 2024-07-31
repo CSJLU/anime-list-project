b388e38960c8d0512db3949776fb821573212558
CSJLU <lucourage@gmail.com>
2024-07-30 22:29:35 -0400

    Add async searching function and jsx to include query inputs

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import AudioPlayer from './AudioPlayer'
 import './Hero.css'
 
 const Hero = () => {
+    const [query, setQuery] = useState('')
+    const [animeData, setAnimeData] = useState([])
+    const navigate = useNavigate()
+
+    /*
+     * Once user submits their query input, async function runs and
+     * attempts to get data at that endpoint. 
+     * When response is successfully processed, animeData
+     * state is changed. 
+     */
+    const HandleAnimeSearch = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(`http://127.0.0.1:5000/search_anime?q=${query}`)
+            const data = await response.json()
+            setAnimeData(data)
+            console.log(data)
+        }
+        catch (error) {
+            console.error(`Error retrieving anime data: ${error}`)
+        }
+    }
+
     return (
         <div className="background-container">
             <div className="card">
-                <form>
+                <form onSubmit={HandleAnimeSearch}>
                     <h1 className="hero-text">Search for your favorite animes here</h1>
                     <div className="search-input">
                         <input
-                            type="search"
+                            type="text"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)} //sets query value from user changing their inputs
                             placeholder="Search for an anime"
                         />
                     </div>
-                    <button>Search</button>
+                    <button type="submit">Search</button>
                 </form>
             </div>
             <AudioPlayer />
