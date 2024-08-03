94160ee53ac98fef2b4d42cef548d773ba1f6db9
CSJLU <lucourage@gmail.com>
2024-08-02 22:03:31 -0400

    Wrap context provider around app and initialize anime data state to be used as values

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,26 @@ import SignUp from './components/SignUp'
 import AnimeInfo from './components/AnimeInfo'
 import './index.css'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import AnimeContext from './components/AnimeContext'
+import { useState } from 'react'
 
 function App() {
+  const [animeData, setAnimeData] = useState([])
   return (
+    <AnimeContext.Provider value={{animeData, setAnimeData}}>
       <div className="app">
         <Router>
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
-            <Route path="/anime" element={<AnimeList />}/>
+            <Route path="/animelist" element={<AnimeList />}/>
             <Route path="/signup" element={<SignUp />}/>
             <Route path="/login" element={<Login />}/>
             <Route path="/animeinfo" element={<AnimeInfo />}/>
           </Routes>
         </Router>
       </div>
+    </AnimeContext.Provider>
   )
 }
 
