9aeb2e25a034b9c01c3a7221581856c93d04c504
CSJLU <lucourage@gmail.com>
2024-08-04 20:15:02 -0400

    Fix anime route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <nav>
                 <ul className="nav-links">
                     <li><a href="/about">About</a></li>
-                    <li><a href="/anime">Animes</a></li>
+                    <li><a href="/animeinfo">Animes</a></li>
                     <li><a href="/signup">Sign Up</a></li>
                     <li><a href="/login">Login</a></li>
                 </ul>
