9b181dcf4530da7cc89ebd0e0c244a0bcea61420
CSJLU <lucourage@gmail.com>
2024-08-04 20:14:37 -0400

    Preliminary design from homepage

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './Header'
+import './Header.css'
+import './Login.css'
 
 const Login = () => {
     return (
-        <h1>Test</h1>
+        <div>
+            <Header />
+            <div className="background-container">
+                <div className="login-card">
+                    <form>
+                        <h1 className="login-text">Login here</h1>
+                        <div className="login-input">
+                            <input
+                                type="text"
+                                placeholder="Search for an anime"
+                            />
+                        </div>
+                        <button type="submit">Log In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './Header'
+import './Header.css'
+import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <h1>Test</h1>
+        <div>
+            <Header />
+            <div className="background-container">
+                <div className="sign-up-card">
+                    <form>
+                        <h1 className="sign-up-text">Sign Up</h1>
+                        <div className="sign-up-input">
+                            <input
+                                type="text"
+                                placeholder="Search for an anime"
+                            />
+                        </div>
+                        <button type="submit">Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
