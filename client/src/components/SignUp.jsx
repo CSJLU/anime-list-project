0d035e070eea4131e768f1ab652250074d688c03
CSJLU <lucourage@gmail.com>
2024-08-06 01:22:29 -0400

    Partial design of sign up page

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faLock, faKey, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import signUpImage from '../assets/lay.jpg'
 import Header from './Header'
-import './Header.css'
-import './SignUp.css'
+import "./SignUp.css"
 
 const SignUp = () => {
     return (
         <div>
-            <Header />
-            <div className="background-container">
-                <div className="sign-up-card">
-                    <form>
-                        <h1 className="sign-up-text">Sign Up</h1>
-                        <div className="sign-up-input">
-                            <input
-                                type="text"
-                                placeholder="Search for an anime"
-                            />
+            <div className="signup-container">
+                <div className="signup-form">
+                    <h2 className="form-title">Sign Up</h2>
+                    <form method="POST">
+                        <div className="form-group">
+                            <FontAwesomeIcon icon={faUser}/>
+                            <label htmlFor="username"></label>
+                            <input type="text" name="username" placeholder="Username"/>
                         </div>
-                        <button type="submit">Sign Up</button>
+                        <div className="form-group">
+                            <FontAwesomeIcon icon={faLock}/>
+                            <label htmlFor="password"></label>
+                            <input type="text" name="password" placeholder="Password"/>
+                        </div>
+                        <div className="form-group">
+                            <FontAwesomeIcon icon={faKey}/>
+                            <label htmlFor="confirm-password"></label>
+                            <input type="text" name="confirm-password" placeholder="Confirm Password"/>
+                        </div>
+                        <div className="form-group">
+                            <FontAwesomeIcon icon={faEnvelope}/>
+                            <label htmlFor="email"></label>
+                            <input type="text" name="email" placeholder="Email"/>
+                        </div>
+                        <button className="register-button" type="submit">Register</button>
                     </form>
                 </div>
             </div>
+        <div className="signup-image">
+            <img src={signUpImage} alt="Anime girl laying down"/>
         </div>
+    </div>
     )
 }
 
