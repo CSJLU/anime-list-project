0575a73a35b9a3d167c4fad5a375203a1aad9143
CSJLU <lucourage@gmail.com>
2024-08-06 21:10:54 -0400

    Side by side form and image

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -9,37 +9,39 @@ const SignUp = () => {
     return (
         <div>
             <div className="signup-container">
-                <div className="signup-form">
-                    <h2 className="form-title">Sign Up</h2>
-                    <form method="POST">
-                        <div className="form-group">
-                            <FontAwesomeIcon icon={faUser}/>
-                            <label htmlFor="username"></label>
-                            <input type="text" name="username" placeholder="Username"/>
-                        </div>
-                        <div className="form-group">
-                            <FontAwesomeIcon icon={faLock}/>
-                            <label htmlFor="password"></label>
-                            <input type="text" name="password" placeholder="Password"/>
-                        </div>
-                        <div className="form-group">
-                            <FontAwesomeIcon icon={faKey}/>
-                            <label htmlFor="confirm-password"></label>
-                            <input type="text" name="confirm-password" placeholder="Confirm Password"/>
-                        </div>
-                        <div className="form-group">
-                            <FontAwesomeIcon icon={faEnvelope}/>
-                            <label htmlFor="email"></label>
-                            <input type="text" name="email" placeholder="Email"/>
-                        </div>
-                        <button className="register-button" type="submit">Register</button>
-                    </form>
+                <div className="signup-content">
+                    <div className="signup-form">
+                        <h2 className="form-title">Sign Up</h2>
+                        <form method="POST">
+                            <div className="form-group">
+                                <FontAwesomeIcon icon={faUser}/>
+                                <label htmlFor="username"></label>
+                                <input type="text" name="username" placeholder="Username"/>
+                            </div>
+                            <div className="form-group">
+                                <FontAwesomeIcon icon={faLock}/>
+                                <label htmlFor="password"></label>
+                                <input type="text" name="password" placeholder="Password"/>
+                            </div>
+                            <div className="form-group">
+                                <FontAwesomeIcon icon={faKey}/>
+                                <label htmlFor="confirm-password"></label>
+                                <input type="text" name="confirm-password" placeholder="Confirm Password"/>
+                            </div>
+                            <div className="form-group">
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                                <label htmlFor="email"></label>
+                                <input type="text" name="email" placeholder="Email"/>
+                            </div>
+                            <button className="register-button" type="submit">Register</button>
+                        </form>
+                    </div>
+                    <div className="signup-image-container">
+                        <img src={signUpImage} className="signup-image" alt="Anime girl laying down"/>
+                    </div>
                 </div>
             </div>
-        <div className="signup-image">
-            <img src={signUpImage} alt="Anime girl laying down"/>
         </div>
-    </div>
     )
 }
 
