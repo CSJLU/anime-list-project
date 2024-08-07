8a7de9f86c94bc940c7e7c9c122de93c46888654
CSJLU <lucourage@gmail.com>
2024-08-06 21:11:40 -0400

    Temporarily remove code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,26 +1,9 @@
 import React from 'react'
-import Header from './Header'
-import './Header.css'
-import './Login.css'
 
 const Login = () => {
     return (
         <div>
-            <Header />
-            <div className="background-container">
-                <div className="login-card">
-                    <form>
-                        <h1 className="login-text">Login here</h1>
-                        <div className="login-input">
-                            <input
-                                type="text"
-                                placeholder="Search for an anime"
-                            />
-                        </div>
-                        <button type="submit">Log In</button>
-                    </form>
-                </div>
-            </div>
+            <h1>Test</h1>
         </div>
     )
 }
