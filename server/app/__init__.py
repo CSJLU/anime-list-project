a74fd38663ce49b052c25b3676a80c83782e8a9c
CSJLU <lucourage@gmail.com>
2024-08-10 20:43:46 -0400

    Encapsulate initialization of app in function and modularize with blueprints, initial database setup

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,30 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
+from .extensions import db
+#from .routes import routes
+from .routes.anime import anime_bp
+import os 
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from app import views
+    db.init_app(app)
+    CORS(app)
+
+
+    #blueprints here
+    #blueprints can replace views.py as its a way to modularize it
+    #for route in routes:
+        #app.register_blueprint(route)
+    
+    app.register_blueprint(anime_bp, url_prefix='/anime')
+
+
+    return app
+
+#from app import views
 
 
