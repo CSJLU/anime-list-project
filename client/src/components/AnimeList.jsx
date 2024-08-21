573f4382621b34ed237b03d53b68b1a1480f19d3
CSJLU <lucourage@gmail.com>
2024-08-21 15:25:58 -0400

    Add unique key and image to mapping

--- client/src/components/AnimeList.jsx
+++ client/src/components/AnimeList.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { useState, useEffect, useContext } from 'react' 
 import AnimeContext from './AnimeContext'
+import Header from './Header'
 
 const AnimeList = () => { 
     const { animeData, setAnimeData } = useContext(AnimeContext)
     return (
-        <div>
-            {animeData.data.map(anime => (
-                <div>
-                    <h3>{anime.title}</h3>
-                </div>
-            ))}
-            <a href="/animeinfo">View more</a>
+        <div className="anime-list-wrapper">
+            <Header />
+            <div className="anime-list-container">
+                {animeData.data.map(anime => (
+                    <div key={anime.mal_id} className="card">
+                        <img src={anime.images.jpg.image_url} alt="Image of anime" className="anime-image"/>
+                        <h3>{anime.title}</h3>
+                    </div>
+                ))}
+                <a href="/animeinfo">View more</a>
+            </div>
         </div>
     )
 
