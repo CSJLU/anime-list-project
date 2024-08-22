a9a3df874b4602064d8c85f20b3616bb1c49feb4
CSJLU <lucourage@gmail.com>
2024-08-22 17:02:21 -0400

    Implement ternary operator for temporary display of undefined animeData

--- client/src/components/AnimeList.jsx
+++ client/src/components/AnimeList.jsx
@@ -3,24 +3,37 @@ import { useState, useEffect, useContext } from 'react'
 import AnimeContext from './AnimeContext'
 import Header from './Header'
 import './AnimeList.css' 
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const AnimeList = () => { 
     const { animeData, setAnimeData } = useContext(AnimeContext)
-    return (
-        <div className="anime-list-wrapper">
-            <Header />
-            <div className="anime-list-container">
-                {animeData.data.map(anime => (
-                    <div key={anime.mal_id} className="card">
-                        <h3>{anime.title}</h3>
-                        <img src={anime.images.jpg.image_url} alt="Image of anime" className="anime-image"/>
-                    </div>
-                ))}
-                <a href="/animeinfo">View more</a>
-            </div>
-        </div>
-    )
+    const { query, setQuery } = useContext(AnimeContext)
+    const navigate = useNavigate()
 
+    return (
+        <>
+            { animeData && animeData.data ? (
+                <div>
+                    <Header />
+                        <div className="anime-list-container">
+                            {animeData.data.map(anime => (
+                                <a href={anime.url}>
+                                    <div key={anime.mal_id} className="card">
+                                        <h3 className="anime-title">{anime.title}</h3>
+                                        <img src={anime.images.jpg.image_url} alt="Image of anime" className="anime-image"/>
+                                    </div>
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+            ) : (
+                <div className="return-container">
+                    <button className="return-button" type="submit">Please click here to return</button>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default AnimeList
