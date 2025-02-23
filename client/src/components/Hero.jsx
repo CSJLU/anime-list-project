48bf5c0dc9e1fc98402864da88648428c94ed072
CSJLU <lucourage@gmail.com>
2025-02-23 14:02:03 -0500

    Fix tabbing and delete unnecessary comments

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -49,10 +49,10 @@ const Hero = () => {
                         <div className="aurora-item"></div>
                         <div className="aurora-item"></div>
                         <div className="aurora-item"></div>
-                        </div>
-                    </h1>
-                    <h2 ref={introRef} className={`intro-text-bottom ${introIsVisible ? "visible" : "hidden"}`}>Search. Create. Display.</h2>
-                    {/*<h2 ref={introRef} className={`${introIsVisible ? "visible" : "hidden"}`}>Choose your Ten</h2>*/}
+                    </div>
+                </h1>
+                <h2 ref={introRef} className={`intro-text-bottom ${introIsVisible ? "visible" : "hidden"}`}>Search. Create. Display.</h2>
+                {/*<h2 ref={introRef} className={`${introIsVisible ? "visible" : "hidden"}`}>Choose your Ten</h2>*/}
             </section>
             <section className="search-container">
                 <div className="hero-card">
